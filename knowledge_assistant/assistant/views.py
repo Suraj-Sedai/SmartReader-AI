9dbaeaaf2bdf10baa422feb9498a2526e33e793c
Suraj Sedai <surajsedai05@gmail.com>
2025-01-24 12:38:46 -0500

    tensorflow setup

--- knowledge_assistant/assistant/views.py
+++ knowledge_assistant/assistant/views.py
@@ -1,12 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from transformers import BertForQuestionAnswering, BertTokenizer
-import torch
+from transformers import TFBertForQuestionAnswering, BertTokenizer
+import tensorflow as tf
 
 # Load the pre-trained model and tokenizer
 model_name = "deepset/bert-base-cased-squad2"  # Pre-trained Q&A model
 tokenizer = BertTokenizer.from_pretrained(model_name)
-model = BertForQuestionAnswering.from_pretrained(model_name)
+model = TFBertForQuestionAnswering.from_pretrained(model_name)
 
 @api_view(['POST'])
 def ask_question(request):
@@ -17,15 +17,18 @@ def ask_question(request):
         return Response({"error": "Both 'question' and 'context' are required."}, status=400)
 
     # Tokenize input
-    inputs = tokenizer(question, context, return_tensors="pt")
-    with torch.no_grad():
-        outputs = model(**inputs)
+    inputs = tokenizer(question, context, return_tensors="tf")
+
+    # Get model predictions
+    outputs = model(inputs)
+    start_logits = outputs.start_logits
+    end_logits = outputs.end_logits
 
     # Extract answer
-    answer_start = torch.argmax(outputs.start_logits)  # Start of answer
-    answer_end = torch.argmax(outputs.end_logits) + 1  # End of answer
+    answer_start = tf.argmax(start_logits, axis=1).numpy()[0]  # Start of answer
+    answer_end = tf.argmax(end_logits, axis=1).numpy()[0] + 1  # End of answer
     answer = tokenizer.convert_tokens_to_string(
-        tokenizer.convert_ids_to_tokens(inputs.input_ids[0][answer_start:answer_end])
+        tokenizer.convert_ids_to_tokens(inputs["input_ids"].numpy()[0][answer_start:answer_end])
     )
 
     return Response({"answer": answer})
